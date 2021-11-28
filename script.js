83d6081ecf95b4fc3cd846b4a39f412316161874
Hack.cpp <72662612+HPD1155@users.noreply.github.com>
2021-11-28 23:49:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function readydownload()
+{
+  var select = document.getElementById('pl');
+  window.filetype = select.options[select.selectedIndex].value;
+}
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+
+
